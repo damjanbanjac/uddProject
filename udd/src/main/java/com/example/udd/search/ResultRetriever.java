a6fb2ad669d4ffa0ed0a034e5c3366ab1af4e3d4
damjanbanjac <damjanbanjac14@gmail.com>
2021-02-22 20:32:11 +0100

    small changes

--- udd/src/main/java/com/example/udd/search/ResultRetriever.java
+++ udd/src/main/java/com/example/udd/search/ResultRetriever.java
@@ -6,21 +6,30 @@ import com.example.udd.Model.ResultData;
 import com.example.udd.handlers.DocumentHandler;
 import com.example.udd.handlers.PDFHandler;
 import com.example.udd.repository.BookIndexRepository;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.document.SearchDocument;
+import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.data.elasticsearch.core.query.*;
+//import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
 
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class ResultRetriever {
+public class ResultRetriever  {
 
     @Autowired
     private BookIndexRepository repository;
+
+
+   // private ElasticsearchTemplate template;
 
     public ResultRetriever(){
     }
@@ -32,7 +41,6 @@ public class ResultRetriever {
         }
 
         List<ResultData> results = new ArrayList<ResultData>();
-
         for (IndexUnit indexUnit : repository.search(query)) {
             results.add(new ResultData(indexUnit.getTitle(), indexUnit.getKeywords(), indexUnit.getFilename(), "",indexUnit.getName(),indexUnit.getSurname(),indexUnit.getGenre()));
         }
@@ -40,6 +48,32 @@ public class ResultRetriever {
 
         return results;
     }
+
+//    public List<ResultData> getHighlihter(org.elasticsearch.index.query.QueryBuilder query,
+//                                       List<RequiredHighlight> requiredHighlights) {
+//        if (query == null) {
+//            return null;
+//        }
+//
+//        List<ResultData> results = new ArrayList<ResultData>();
+//
+//     //   SearchQuery search = new SearchQuery();
+//        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+//                .withQuery(query)
+//                .withHighlightFields(new HighlightBuilder.Field("title"))
+//                .build();
+//
+//
+//
+//        List<IndexUnit> indexUnits = template.queryForList(searchQuery, IndexUnit.class);
+//
+//        for (IndexUnit indexUnit : indexUnits) {
+//            results.add(new ResultData(indexUnit.getTitle(), indexUnit.getKeywords(), indexUnit.getFilename(), ""));
+//        }
+//
+//
+//        return results;
+//    }
 
     protected DocumentHandler getHandler(String fileName){
         if(fileName.endsWith(".pdf")){
